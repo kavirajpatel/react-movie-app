0a3ab191d3e38c8f465068c9e1f1095036048ccc
kavirajpatel <142300937+kavirajpatel@users.noreply.github.com>
2023-11-29 15:46:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import reportWebVitals from "./component/reportWebVitals";
+import MoviesApp from "./MoviesApp";
+
+function App() {
+  return (
+    <div className="App">
+     <MoviesApp/>
+    </div>
+  );
+}
+
+export default App;
